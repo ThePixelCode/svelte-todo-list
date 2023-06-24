9d60210d05d003a5ba2cf834bac73e2c14b6e447
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2023-06-23 19:19:46 -0500

    created api file

---
+++ src/routes/api/v1/task/+server.ts
@@ -0,0 +1,21 @@
+import { error, type RequestHandler } from '@sveltejs/kit';
+
+export const GET = (() => {
+	throw error(418, 'Not yet implemented');
+}) satisfies RequestHandler;
+
+export const POST = (() => {
+	throw error(418, 'Not yet implemented');
+}) satisfies RequestHandler;
+
+export const PUT = (() => {
+	throw error(418, 'Not yet implemented');
+}) satisfies RequestHandler;
+
+export const PATCH = (() => {
+	throw error(418, 'Not yet implemented');
+}) satisfies RequestHandler;
+
+export const DELETE = (() => {
+	throw error(418, 'Not yet implemented');
+}) satisfies RequestHandler;
