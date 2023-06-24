77618e3ea123e7b334d16bf28417120f32952c92
misterbug5 <72231627+misterbug5@users.noreply.github.com>
2023-06-23 19:22:38 -0500

    Added TODOs

--- src/routes/api/v1/task/+server.ts
+++ src/routes/api/v1/task/+server.ts
@@ -1,21 +1,26 @@
 import { error, type RequestHandler } from '@sveltejs/kit';
 
 export const GET = (() => {
+	// TODO: Implement logic
 	throw error(418, 'Not yet implemented');
 }) satisfies RequestHandler;
 
 export const POST = (() => {
+	// TODO: Implement logic
 	throw error(418, 'Not yet implemented');
 }) satisfies RequestHandler;
 
 export const PUT = (() => {
+	// TODO: Implement logic
 	throw error(418, 'Not yet implemented');
 }) satisfies RequestHandler;
 
 export const PATCH = (() => {
+	// TODO: Implement logic
 	throw error(418, 'Not yet implemented');
 }) satisfies RequestHandler;
 
 export const DELETE = (() => {
+	// TODO: Implement logic
 	throw error(418, 'Not yet implemented');
 }) satisfies RequestHandler;
