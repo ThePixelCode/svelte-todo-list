bb486572dc8c705ff3aae925e49c55ee6172f72b
ThePixelCode <72231627+thepixelcode@users.noreply.github.com>
2023-06-27 02:56:25 -0500

    GET created

--- src/routes/api/v1/task/+server.ts
+++ src/routes/api/v1/task/+server.ts
@@ -1,8 +1,23 @@
-import { error, type RequestHandler } from '@sveltejs/kit';
+import { prisma } from '$lib/server/prisma';
+import { error, json, type RequestHandler } from '@sveltejs/kit';
 
-export const GET = (() => {
-	// TODO: Implement logic
-	throw error(418, 'Not yet implemented');
+const pageSize = 10;
+
+export const GET = (async ({ url }) => {
+	const idString = url.searchParams.get('id');
+	const pageUncheckedNumber = Number(url.searchParams.get('page') ?? '1');
+	if (isNaN(pageUncheckedNumber)) return json({ error: 'invalid page' }, { status: 400 });
+	const page = Math.floor(pageUncheckedNumber);
+	const id = idString != null ? Number(idString) : idString;
+	if (id != null && isNaN(id)) return json({ error: 'invalid id' }, { status: 400 });
+	const result = await prisma.task.findMany({
+		where: {
+			id: id != null ? id : undefined
+		},
+		skip: (page - 1) * pageSize,
+		take: pageSize
+	});
+	return json(result, { status: 200 });
 }) satisfies RequestHandler;
 
 export const POST = (() => {
